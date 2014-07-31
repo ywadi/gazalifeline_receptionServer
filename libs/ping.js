1820e34be0b3b37bd9383b06c19309b888bea75b
root <root@Gazalifeline-service.(none)>
2014-07-31 11:13:34 +0000

    Life ping completed

--- libs/ping.js
+++ libs/ping.js
@@ -1,13 +1,28 @@
+var redis = require("redis"), client = redis.createClient();
+
+client.on("error", function (err) {
+        //Track errors on Console.log -> Forever logs 
+        console.log("Error: " + err);
+    });
+
 function ping(phoneNumber, callback)
 {
-	//TODO: Write to Redis, HSET with a key life of 5 minutes, key contains last timestamp. On Success return true
-	callback("Done On -> "+phoneNumber);
+	//Write to Redis, HSET with a key life of 5 minutes, key contains last timestamp. On Success return true
+	var pingDate = new Date(); //Server Date and time are UTC 
+	client.hset(phoneNumber, "lastDate", pingDate, function(err,reply){
+	    client.expire(phoneNumber,5*60);
+	    callback("Pong: "+phoneNumber);    
+	});
+	
 }
 
 function getLive(callback)
 {
-	//TODO: Read all live keys here on Redis and send back through callback
-	callback("DATA HERE");
+	//Read all live keys here on Redis and send back through callback
+	client.keys("*", function (err, replies) {
+        callback(replies);
+    }); 
+	
 }
 
 exports.ping = ping;
